384982ae5f853f689bd47a0ce0c8b6849b9934a3
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-08-06 09:17:19 +0800

    :ambulance: Build clean bin files

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f06d8bec0271845752e1a02b6f3a7dd5f7cb79c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24801a0b7e4bbc51d4bf0cb6b0f46f27d66acd6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
